c4e67a457debb8f5b6ff0c9159b51ee8553d617f
Mahima Agarwal <mahimaagarwal263@gmail.com>
2023-04-07 11:34:35 +0530

    parking simulator setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Parking simulator using JAVA(Using Concurrency and Multithreading)
+Implementing an application simulating a parking area, with sensors that detect the following: when a car or a motorcycle enters or goes out of the parking area, an object to store the statistics of the vehicles being parked, and a mechanism to control cash flow.
+
+Implement two version of the application: one without any synchronization mechanisms(Main.java), where we will obtain incorrect results, and one that works correctly(SynCarParking.jsvs) as it uses the two variants of the synchronized keyword.
+
+
+
---
+++ SyncCarParking.java
@@ -0,0 +1,144 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+class SyncCarParking {
+    public static void main(String[] args) {
+        
+        ParkingCash cash = new ParkingCash(); 
+        ParkingStats stats = new ParkingStats(cash);
+
+        System.out.printf("Parking Simulator\n");
+        
+        int numberSensors=2 * Runtime.getRuntime()
+                                           .availableProcessors(); 
+        
+        System.out.printf("No of sensor: "+numberSensors+"\n");
+        
+        Thread threads[]=new Thread[numberSensors]; 
+        for (int i = 0; i<numberSensors; i++) { 
+          Sensor sensor=new Sensor(stats); 
+          Thread thread=new Thread(sensor); 
+          thread.start(); 
+          threads[i]=thread; 
+        }
+        
+         for (int i=0; i<numberSensors; i++) { 
+          try { 
+            threads[i].join(); 
+          } catch (InterruptedException e) { 
+            e.printStackTrace(); 
+          } 
+        }
+        
+         System.out.printf("Number of cars: %d\n",
+                              stats.getNumberCars()); 
+            System.out.printf("Number of motorcycles: %d\n",
+                               stats.getNumberMotorcycles()); 
+            cash.close(); 
+    }
+}
+
+class ParkingCash{
+    private static final int cost = 2;
+    private long cash;
+    
+    public ParkingCash(){
+        cash =0;
+    }
+    
+    public synchronized void  vehiclePay() { 
+          cash+=cost; 
+    }
+    
+     public void close() { 
+        System.out.printf("Closing accounting"); 
+          long totalAmmount; 
+          synchronized (this) { 
+            totalAmmount=cash; 
+            cash=0; 
+          } 
+          System.out.printf("The total amount is : %d",totalAmmount);  
+     }          
+}
+
+class ParkingStats { 
+    private long numberCars; 
+    private long numberMotorcycles; 
+    private ParkingCash cash;
+    private final Object controlCars, controlMotorcycles; 
+        
+    public ParkingStats (ParkingCash cash) { 
+          numberCars=0; 
+          numberMotorcycles=0; 
+          controlCars=new Object(); 
+          controlMotorcycles=new Object(); 
+          this.cash=cash; 
+        }
+    
+    public void carComeIn() { 
+          synchronized (controlCars) { 
+            numberCars++; 
+          } 
+        }
+    public void carGoOut() { 
+        synchronized (controlCars) { 
+        numberCars--; 
+    } 
+        cash.vehiclePay(); 
+    }
+    
+    public void motoComeIn() { 
+         synchronized (controlMotorcycles) { 
+            numberMotorcycles++; 
+          } 
+    }
+    
+    public void motoGoOut() { 
+        synchronized (controlMotorcycles) { 
+            numberMotorcycles--; 
+    }    
+        cash.vehiclePay(); 
+    }
+    
+    public long getNumberMotorcycles() { 
+        return  numberMotorcycles;
+    }
+    
+    public long getNumberCars() { 
+        return  numberCars;
+    }
+}
+
+class Sensor implements Runnable {
+    private ParkingStats stats;
+    
+      @Override 
+        public void run() { 
+          for (int i = 0; i< 10; i++) { 
+            stats.carComeIn(); 
+            stats.carComeIn(); 
+            try { 
+              TimeUnit.MILLISECONDS.sleep(50); 
+            } catch (InterruptedException e) { 
+              e.printStackTrace(); 
+            } 
+            stats.motoComeIn(); 
+            try { 
+              TimeUnit.MILLISECONDS.sleep(50); 
+            } catch (InterruptedException e) { 
+              e.printStackTrace(); 
+            }
+             stats.motoGoOut(); 
+            stats.carGoOut(); 
+            stats.carGoOut(); 
+          } 
+        }
+    
+    public Sensor(ParkingStats stats) { 
+        this.stats = stats; 
+    }
+}
+
+
